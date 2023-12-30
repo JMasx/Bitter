7bcdd76cdf7b2c4f6106bab0711daf641f63870d
RomanOmni <motomike09@gmail.com>
2023-12-29 17:31:55 -0700

    top-down movement
    movement needs smoothing and rework to have correct registers. Try this site: https://www.gdquest.com/tutorial/godot/2d/top-down-movement/

--- player.gd
+++ player.gd
@@ -2,35 +2,41 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+#const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 ####SHOULD BE ABLE TO REMOVE GRAVITY
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#---var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimatedSprite2D/AnimationPlayer")
 
 ####SHOULD BE ABLE TO REMOVE PHYSICS PROCESS COMPLETELY
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+#---	if not is_on_floor():
+#---		velocity.y += gravity * delta
 
 ####REMOVE JUMP
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+#---	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#---		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 ####INTEGRATE VERTICLE INPUT DIRECTIONS
 ####IMPLEMENT WAY FOR SPRITE TO CHANGE DIR BASED ON INPUT
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction1 = Input.get_axis("ui_left", "ui_right")
+	var direction2 = Input.get_axis("ui_up", "ui_down")
 ####INTEGRATE SPRINTING AND CREATE CASE FOR RUNNING ANNIMATION
-	if direction:
-		velocity.x = direction * SPEED
+	if direction1:
+		velocity.x = direction1 * SPEED
+		anim.play("walk")
+	elif direction2:
+		velocity.y = direction2 * SPEED
 		anim.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 		anim.play("idle")
+	
 
 	move_and_slide()
